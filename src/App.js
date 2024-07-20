96d9e420a0fc131678687babb0868c5101b24b12
leejeayoung <leejeayoung875@gmail.com>
2024-07-20 23:58:16 +0900

    todolist 1차 형태

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 function APP(){
+    const [state,setState]=useState([]);
+    const [inputValue,setInput]=useState('');
+
+    const addList=()=>{
+        if(inputValue.trim()){
+            setState([...state,{text:inputValue,complete:false}]);
+            setInput('');
+        }
+    }
     return(
         <div>
-            <h1>test</h1>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e)=>setInput(e.target.value)}
+            />
+            <button onClick={addList}>추가</button>
+            <ul>
+                {state.map((stae,index)=>(
+                    <li key={index}>
+                    {stae.text}
+                    </li>
+                ))}
+                </ul>
+            
         </div>
     )
 }
